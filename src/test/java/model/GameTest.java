806d062dc879fd5ef57837bf2339d4fa7adde9be
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-03-23 11:50:57 +0100

    Update GameTest.java
    Added testing for endTurn method

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1,7 +1,13 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    @Test
+    void endTurn() {
+        assertEquals (2, 1+1);
+    }
 }
