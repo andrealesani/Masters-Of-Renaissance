e23b55c17c49de58b1fa1f651731339e46243e22
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-03-28 01:29:44 +0100

    Update Production.java and ProductionHandler.java
    Added methods following the UML design changes

--- src/main/java/model/Production.java
+++ src/main/java/model/Production.java
@@ -17,27 +17,22 @@ public class Production {
     private Map<Resource, Integer> output = new HashMap<>();
     private boolean selectedByHandler;
 
-    public Map<Resource, Integer> getInput() {
-        return input;
-    }
-
-    public void setInput(Map<Resource, Integer> input) {
+    public Production(Map<Resource, Integer> input, Map<Resource, Integer> output){
         this.input = input;
-    }
-
-    public Map<Resource, Integer> getOutput() {
-        return output;
-    }
-
-    public void setOutput(Map<Resource, Integer> output) {
         this.output = output;
     }
+
+    public Map<Resource, Integer> getInput() { return input; }
+
+    public void setInput(Map<Resource, Integer> input) { this.input = input; }
+
+    public Map<Resource, Integer> getOutput() { return output; }
+
+    public void setOutput(Map<Resource, Integer> output) { this.output = output; }
 
     public boolean isSelectedByHandler() { return this.selectedByHandler; }
 
     public void select() { this.selectedByHandler = true; }
 
-    public void unselect() {
-        this.selectedByHandler = false;
-    }
+    public void unselect() { this.selectedByHandler = false; }
 }
--- src/main/java/model/ProductionHandler.java
+++ src/main/java/model/ProductionHandler.java
@@ -14,7 +14,8 @@ import java.util.Map;
  */
 public class ProductionHandler {
     private List<Production> productions = new ArrayList<>();
-    private Map<Resource, Integer> currentCost= new HashMap<>();
+    private Map<Resource, Integer> currentInput= new HashMap<>();
+    private Map<Resource, Integer> currentOutput= new HashMap<>();
 
     public void addProduction(Production production){
         productions.add(production);
@@ -24,30 +25,42 @@ public class ProductionHandler {
         productions.remove(production);
     }
 
+    public void chooseJollyInput(Resource resource){
+        //TODO
+    }
+
+    public void chooseJollyOutput(Resource resource){
+        //TODO
+    }
+
     /**
      * Marks the specified Production as selected. At the end of the turn, only selected Productions will be activated
-     * @param i
+     * @param i - indicates the position of the element in the List
      */
     public void selectProduction(int i){
         productions.get(i).select();
     }
 
-    /**
-     * Returns the amount of the specified Resource that is needed to activate the selected Productions
-     * @param resource
-     * @return
-     */
-    public int getTotalCost(Resource resource){
-        //TODO
-        return 0;
+    public void resetProductionChoice(){
+        for (Production production: productions) {
+            production.unselect();
+        }
     }
 
     /**
      * Activates the selected Productions. In order to do this, the method removes all the input Resources and adds all
      * the output Resources specified in the selected Productions to the specified PlayerBoard
-     * @param playerBoard
+     * @param playerBoard - indicates which player is activating the Productions
      */
-    public void activateTransaction(PlayerBoard playerBoard){
+    public void confirmProductionChoice(PlayerBoard playerBoard){
+        //TODO
+    }
+
+    public void takeResourceFromWarehouse(Resource resource, int quantity){
+        //TODO
+    }
+
+    public void takeResourceFromStrongbox(Resource resource, int quantity){
         //TODO
     }
 }
