1a79bd939d14d760ff1123bc08ae108ccf350882
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-03-28 01:28:26 +0100

    Update DevelopmentCard.java
    Added constructor and some other methods that were missing from UML

--- src/main/java/model/card/DevelopmentCard.java
+++ src/main/java/model/card/DevelopmentCard.java
@@ -1,22 +1,33 @@
 package model.card;
 
+import model.PlayerBoard;
 import model.Production;
 import model.resource.Resource;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/* NOTE: in order to fully respect the purpose of this class, we could make all attributes final
+ * and only set them once in the constructor */
 public class DevelopmentCard extends Card {
-    private int level;
-    private int color; /* change color to enum? */
-    private Map<Resource, Integer> cost = new HashMap<>();
-    private Production production;
+    private final int level;
+    private final int color; /* change color to enum? */
+    private final Map<Resource, Integer> cost = new HashMap<>();
+    private final Production production;
 
-    public int getLevel() {
-        return level;
+    public DevelopmentCard(int level, int color, Production production) {
+        this.level = level;
+        this.color = color;
+        this.production = production;
     }
 
-    public int getColor() {
-        return color;
+    public int getLevel() { return level; }
+
+    public int getColor() { return color; }
+
+    public Production getProduction(){ return production; }
+
+    public void addTo(int quantity, PlayerBoard playerBoard){
+        //TODO
     }
 }
