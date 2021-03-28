00ac87e64bb886481f47105618dd8ce81b89a6d1
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-03-28 23:36:48 +0200

    Create UnlimitedStorage.java

---
+++ src/main/java/model/storage/UnlimitedStorage.java
@@ -0,0 +1,17 @@
+package model.storage;
+
+import model.resource.Resource;
+
+/**
+ * This class represents a type of resource container that can store unlimited amounts of every resource.
+ * It is used to model the strongbox and resource waiting room in the player board.
+ */
+public class UnlimitedStorage extends ResourceStash {
+    /**
+     * Adds the given resource to storageContent
+     * @param resource - the resource to be added
+     */
+    public void addResource (Resource resource) {
+        //TODO
+    }
+}
