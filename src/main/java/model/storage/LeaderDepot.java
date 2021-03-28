1c654a8e7f2124d616870de72c2458addcf69dc9
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-03-29 00:24:17 +0200

    Create LeaderDepot.java

---
+++ src/main/java/model/storage/LeaderDepot.java
@@ -0,0 +1,44 @@
+package model.storage;
+
+import model.resource.Resource;
+
+/**
+ * This class represents a depot of the kind obtained after activating certain leader cards, providing the player with additional warehouse space.
+ * This kind of depot can usually contain only two pieces of a single type of resource, and is not counted when determining resource blocking for basic depots.
+ */
+public class LeaderDepot {
+    /**
+     * Adds the given resource to storageContent
+     * @param resource - the resource to be added
+     */
+    public void addResource (Resource resource){
+        //TODO
+    }
+    /**
+     * Returns whether or not the depot, if it were empty, could contain the given amount of the given resource
+     * @param resource - the resource to be stored
+     * @param quantity - the amount to be stored of the given resource
+     * @return true if the given resource and amount could be contained in the depot
+     */
+    public boolean canHold (Resource resource, int quantity){
+        //TODO
+        return false;
+    }
+
+    /**
+     * Returns false at all times, as this type of depot can never block a resource
+     * @param resource - the resource that might be blocked
+     * @return false
+     */
+    public boolean isBlocking (Resource resource){
+        //TODO
+        return false;
+    }
+
+    /**
+     * Empties the depot of its entire content
+     */
+    public void empty(){
+        //TODO
+    }
+}
