418368597ed26fd8bfc38ec09b45c9c70ecc21b0
Giggi98 <80157635+Giggi98@users.noreply.github.com>
2021-03-30 15:20:05 +0100

    Create Market.java

---
+++ src/main/java/model/Market.java
@@ -0,0 +1,18 @@
+package model;
+
+import model.resource.Resource;
+
+public class Market {
+    private Resource board[][];
+    private Resource slideOrb;
+
+    public void selectedResources(MarketScope marketScope, int numScope, PlayerBoard playerBoard) {
+        //TODO
+    }
+
+
+    public void shiftResources (MarketScope marketScope,int numScope){
+        //TODO
+    }
+
+}
