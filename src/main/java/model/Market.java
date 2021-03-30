4ee36e1ced521c53f9f63e5e938a7cd42d5b6fae
Giggi98 <80157635+Giggi98@users.noreply.github.com>
2021-03-30 18:17:32 +0100

    Create Market Setup

--- src/main/java/model/Market.java
+++ src/main/java/model/Market.java
@@ -1,17 +1,64 @@
 package model;
 
-import model.resource.Resource;
+import model.resource.*;
 
 public class Market {
-    private Resource board[][];
+    private Resource board[][] = new Resource[3][4];
     private Resource slideOrb;
+
+    public Market() {
+        int totalOrbs = 13;
+        int orb = 0;
+        int countRS = 0, countRF = 0, countRSe = 0, countRSh = 0, countRSt = 0, countRW = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                orb = (int) (Math.random() * 6);
+                if ((orb == 0) && (countRS < 2)) {
+                    board[i][j] = new ResourceCoin();
+                    countRS++;
+                }
+                else if ((orb == 1) && (countRF < 1)) {
+                    board[i][j] = new ResourceFaith();
+                    countRF++;
+                }
+                else if ((orb == 2) && (countRSe < 2)) {
+                    board[i][j] = new ResourceServant();
+                    countRSe++;
+                }
+                else if ((orb == 3) && (countRSh < 2)) {
+                    board[i][j] = new ResourceShield();
+                    countRSh++;
+                }
+                else if ((orb == 4) && (countRSt < 2)) {
+                    board[i][j] = new ResourceStone();
+                    countRSt++;
+                }
+                else if ((orb == 5) && (countRW < 4)) {
+                    board[i][j] = new ResourceWhite();
+                    countRW++;
+                }
+
+                totalOrbs--;
+
+            }
+        }
+    }
+
+    public Resource[][] getBoard() {
+        return board;
+    }
+
+    public Resource getMarble(int row, int column) {
+        return board[row][column];
+    }
 
     public void selectedResources(MarketScope marketScope, int numScope, PlayerBoard playerBoard) {
         //TODO
     }
 
 
-    private void shiftResources (MarketScope marketScope,int numScope){
+    private void shiftResources(MarketScope marketScope, int numScope) {
         //TODO
     }
 
