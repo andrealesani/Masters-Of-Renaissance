d88e20bf773687e4719c7a80251522594e860a99
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-02 19:17:39 +0200

    Updated PlayerBoard Javadoc

--- src/main/java/model/PlayerBoard.java
+++ src/main/java/model/PlayerBoard.java
@@ -20,17 +20,19 @@ public class PlayerBoard {
     private Game game;
     private String username;
     private int faith;
-    private List<PopeTileState> popeFavorTiles = new ArrayList<>();
-    private Warehouse warehouse = new Warehouse();
-    private ResourceStash strongbox = new ResourceStash();
-    private ResourceStash waitingRoom = new ResourceStash();
-    private List<Resource> marbleConversions = new ArrayList<>();
-    private Map<Resource, Integer> discounts = new HashMap<>();
-    private List<List<DevelopmentCard>> cardSlots = new ArrayList<List<DevelopmentCard>>();
-    private List<LeaderCard> leaderCards = new ArrayList<>();
-    private ProductionHandler productionHandler = new ProductionHandler();
+    private final List<PopeTileState> popeFavorTiles = new ArrayList<>();
+    private final Warehouse warehouse = new Warehouse();
+    private final ResourceStash strongbox = new ResourceStash();
+    private final ResourceStash waitingRoom = new ResourceStash();
+    private final List<Resource> marbleConversions = new ArrayList<>();
+    private final Map<Resource, Integer> discounts = new HashMap<>();
+    private final List<List<DevelopmentCard>> cardSlots = new ArrayList<List<DevelopmentCard>>();
+    private final List<LeaderCard> leaderCards = new ArrayList<>();
+    private final ProductionHandler productionHandler = new ProductionHandler();
 
     /**
+     * Getter
+     *
      * @return - returns the player's warehouse
      */
     public Warehouse getWarehouse() {
@@ -54,8 +56,9 @@ public class PlayerBoard {
     }
 
     /**
-     * This method adds the specified DevelopmentCard to the specified slot in the player's PlayerBoard
-     * @param slot - specifies to which of the 3 production slots the Card must be added
+     * Adds the specified DevelopmentCard to the specified slot in the player's PlayerBoard
+     *
+     * @param slot            - specifies to which of the 3 production slots the Card must be added
      * @param developmentCard - card that must be added
      */
     public void addDevelopmentCard(int slot, DevelopmentCard developmentCard) {
@@ -67,7 +70,8 @@ public class PlayerBoard {
     }
 
     /**
-     * This method adds the specified LeaderCard to the player's leaderCards list
+     * Adds the specified LeaderCard to the player's leaderCards list
+     *
      * @param leaderCard - specifies the LeaderCard to add
      */
     public void addLeaderCard(LeaderCard leaderCard) {
@@ -75,19 +79,21 @@ public class PlayerBoard {
     }
 
     /**
-     * This method checks if the player can activate a certain LeaderCard and activates it
+     * Checks if the player can activate a certain LeaderCard and activates it
+     *
      * @param i - specifies the position of the LeaderCard in the leaderCards list
      * @throws RequirementsNotMetException - thrown if the player does not fulfill the requirements to activate the specified LeaderCard
      */
-    public void playLeaderCard(int i) throws RequirementsNotMetException{
+    public void playLeaderCard(int i) throws RequirementsNotMetException {
         if (leaderCards.get(i).areRequirementsMet(this)) {
             leaderCards.get(i).doAction(this);
         } else throw new RequirementsNotMetException();
     }
 
     /**
-     * This method adds the specified Resource to the marbleConversion list so that the player can convert a WhiteOrb
+     * Adds the specified Resource to the marbleConversion list so that the player can convert a WhiteOrb
      * into the Resource whenever he picks it from the Market
+     *
      * @param resource - Resource (specified in the LeaderCard) that the WhiteOrb can be transformed into from now on
      */
     public void addMarbleConversion(Resource resource) {
@@ -95,8 +101,9 @@ public class PlayerBoard {
     }
 
     /**
-     * This method adds the specified Production to the ProductionHandler so that the player can activate it just like
+     * Adds the specified Production to the ProductionHandler so that the player can activate it just like
      * any other Production he already owns
+     *
      * @param production - Production (specified in the LeaderCard) that can be activated from now on
      */
     public void addProduction(Production production) {
@@ -104,8 +111,9 @@ public class PlayerBoard {
     }
 
     /**
-     * This method adds the specified discount to the discounts list so that the player can pay less
+     * Adds the specified discount to the discounts list so that the player can pay less
      * Resources when buying DevelopmentCards
+     *
      * @param resource - specifies the Resource affected by the discount
      * @param discount - specifies the amount of Resources discounted
      */
@@ -115,6 +123,7 @@ public class PlayerBoard {
 
     /**
      * This method is called when a player decides to discard one of his two LeaderCards in order to get one faith point
+     *
      * @param i - specifies the position of the LeaderCard in the leaderCards list
      */
     public void discardLeaderCard(int i) {
@@ -134,7 +143,6 @@ public class PlayerBoard {
     }
 
     /**
-     *
      * @param resource - specifies the Resource type to count
      * @return - returns the total amount of the player's Resources distributed both in his strongbox and his warehouse
      */
@@ -144,16 +152,15 @@ public class PlayerBoard {
     }
 
     /**
-     *
      * @param cardColor - specifies the CardColor of the cards to count
-     * @param level - specifies the level of the card to count
+     * @param level     - specifies the level of the card to count
      * @return - returns the total number of DevelopmentCards owned by the player that fulfill both the color and the level requirements
      */
     public int getNumOfCards(CardColor cardColor, int level) {
         int num = 0;
-        for (List<DevelopmentCard> deck: cardSlots) {
-            for (DevelopmentCard developmentCard: deck) {
-                if(developmentCard.getColor() == cardColor && developmentCard.getLevel() == level){
+        for (List<DevelopmentCard> deck : cardSlots) {
+            for (DevelopmentCard developmentCard : deck) {
+                if (developmentCard.getColor() == cardColor && developmentCard.getLevel() == level) {
                     num++;
                 }
             }
@@ -163,6 +170,7 @@ public class PlayerBoard {
 
     /**
      * atm this method is super dumb but robust. It depends on the method above
+     *
      * @param cardColor - specifies the CardColor of the cards to count
      * @return returns the total number of DevelopmentCards owned by the player that fulfill the color requirement
      */
