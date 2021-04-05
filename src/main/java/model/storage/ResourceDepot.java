6c1c515a45d18d0cf75035073cc348561b28f8c7
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-04-05 12:35:22 +0200

    Update ResourceDepot.java
    Changed methods signature

--- src/main/java/model/storage/ResourceDepot.java
+++ src/main/java/model/storage/ResourceDepot.java
@@ -1,25 +1,32 @@
 package model.storage;
 
-import model.resource.Resource;
+import model.ResourceType;
 
 /**
  * This interface indicates that the implementing class can be used as a depot in the player board warehouse.
+ * It has methods for evaluating the depot's capacity or whether it is blocking a specific resource, and to empty the depot, as well as the standard methods for a ResourceStash.
  */
-public interface ResourceDepot {
+public interface ResourceDepot extends ResourceStash {
     /**
      * Returns whether or not the depot, if it were empty, could contain the given amount of the given resource
      * @param resource - the resource to be stored
      * @param quantity - the amount to be stored of the given resource
      * @return true if the given resource and amount could be contained in the depot
      */
-    boolean canHold (Resource resource, int quantity);
+    boolean canHold (ResourceType resource, int quantity);
 
     /**
      * Returns whether or not the depot is blocking a certain resource, meaning that no other depot of the basic type can contain it
      * @param resource - the resource that might be blocked
      * @return true if the given resource is blocked by this depot
      */
-    boolean isBlocking (Resource resource);
+    boolean isBlocking (ResourceType resource);
+
+    /**
+     * Returns the maximum number of resources that can be stored in the depot
+     * @return the size of the depot
+     */
+    int getSize ();
 
     /**
      * Empties the depot of its entire content
