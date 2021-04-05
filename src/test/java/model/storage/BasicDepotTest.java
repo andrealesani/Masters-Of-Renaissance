168db927070d6cb793344575e872e5d7b27fbfd5
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-04-05 12:35:52 +0200

    Create BasicDepotTest.java

---
+++ src/test/java/model/storage/BasicDepotTest.java
@@ -0,0 +1,48 @@
+package model.storage;
+
+import model.ResourceType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicDepotTest {
+
+    /**
+     * This method tests the addition of resources to the storage
+     */
+    @Test
+    void addResource() {
+    }
+
+    @Test
+    void canHold() {
+    }
+
+
+    @Test
+    void isBlocking() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void empty() {
+    }
+
+    /**
+     * This method tests the removal of resources from the storage
+     */
+    @Test
+    void removeResource() {
+    }
+
+    @Test
+    void getNumOfResource() {
+    }
+
+    @Test
+    void getStoredResources() {
+    }
+}
