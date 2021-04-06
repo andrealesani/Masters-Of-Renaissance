ac07d7c032e568c89a80b5ed51516b6a4645f212
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-06 18:44:22 +0200

    Update CardTable.java

--- src/main/java/model/CardTable.java
+++ src/main/java/model/CardTable.java
@@ -10,16 +10,55 @@ public class CardTable {
     private List<List<DevelopmentCard>> yellowCards;
     private List<List<DevelopmentCard>> purpleCards;
 
-    public void buyTopCard(CardColor color, int position, int idk, PlayerBoard playerBoard){
+    /**
+     * Adds the selected card to the specified PlayerBoard in the specified slot
+     *
+     * @param column      specifies the column of the card in the grid
+     * @param row         specifies the row of the card in the grid
+     * @param playerBoard specifies which player is buying the card
+     * @param cardSlot    specifies in which production slot the player wants to put the card
+     */
+    public void buyTopCard(CardColor column, int row, PlayerBoard playerBoard, int cardSlot) {
+        List<List<DevelopmentCard>> deckColumn = null;
+        if (column.equals(CardColor.GREEN))
+            deckColumn = greenCards;
+        else if (column.equals(CardColor.BLUE))
+            deckColumn = blueCards;
+        else if (column.equals(CardColor.YELLOW))
+            deckColumn = yellowCards;
+        else if (column.equals(CardColor.PURPLE))
+            deckColumn = purpleCards;
+
+        playerBoard.addDevelopmentCard(cardSlot, deckColumn.get(row).get(0));
+    }
+
+    public void discardTop(CardColor color, int idk) {
         //TODO
     }
 
-    public void discardTop(CardColor color, int idk){
-        //TODO
-    }
+    /**
+     * Checks that there's at least one card of each color still available (this method should be used only in solo mode)
+     *
+     * @return true if there's at least one card of each color still available
+     */
+    public boolean checkAllColorAvailable() {
+        for (List<DevelopmentCard> deck : greenCards) {
+            if (deck.size() < 1)
+                return false;
+        }
+        for (List<DevelopmentCard> deck : blueCards) {
+            if (deck.size() < 1)
+                return false;
+        }
+        for (List<DevelopmentCard> deck : yellowCards) {
+            if (deck.size() < 1)
+                return false;
+        }
+        for (List<DevelopmentCard> deck : purpleCards) {
+            if (deck.size() < 1)
+                return false;
+        }
 
-    public boolean checkAllColorAvailable(){
-        //TODO
-        return false;
+        return true;
     }
 }
