3b2207579a35585eb1fe18f43eaf03738785f5b6
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-04-06 12:40:22 +0200

    Update BasicDepotTest.java
    Filled in methods

--- src/test/java/model/storage/BasicDepotTest.java
+++ src/test/java/model/storage/BasicDepotTest.java
@@ -1,5 +1,9 @@
 package model.storage;
 
+import Exceptions.NotEnoughResourceException;
+import Exceptions.NotEnoughSpaceException;
+import Exceptions.ResourceNotPresentException;
+import Exceptions.WrongResourceTypeException;
 import model.ResourceType;
 import org.junit.jupiter.api.Test;
 
@@ -12,23 +16,84 @@ class BasicDepotTest {
      */
     @Test
     void addResource() {
+        ResourceDepot stash = new BasicDepot(3);
+        try {
+            stash.addResource(ResourceType.SHIELD, 1);
+        } catch (NotEnoughSpaceException | WrongResourceTypeException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        assertEquals(1, stash.getNumOfResource(ResourceType.SHIELD));
     }
 
+    /**
+     * This method tests the holding capacity of the storage
+     */
     @Test
     void canHold() {
+        ResourceDepot stash = new BasicDepot(3);
+        try {
+            stash.addResource(ResourceType.SHIELD, 1);
+        } catch (NotEnoughSpaceException | WrongResourceTypeException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        assertTrue(stash.canHold(ResourceType.COIN, 2));
     }
 
-
+    /**
+     * This method test the storage's blocking mechanic for resources in the affirmative case
+     */
     @Test
-    void isBlocking() {
+    void isBlockingTrue() {
+        ResourceDepot stash = new BasicDepot(3);
+        try {
+            stash.addResource(ResourceType.SHIELD, 1);
+        } catch (NotEnoughSpaceException | WrongResourceTypeException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        assertTrue(stash.isBlocking(ResourceType.SHIELD));
     }
 
+    /**
+     * This method test the storage's blocking mechanic for resources in the negative case
+     */
+    @Test
+    void isBlockingFalse() {
+        ResourceDepot stash = new BasicDepot(3);
+        try {
+            stash.addResource(ResourceType.SHIELD, 1);
+        } catch (NotEnoughSpaceException | WrongResourceTypeException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        assertFalse(stash.isBlocking(ResourceType.COIN));
+    }
+
+    /**
+     * This method tests the storage's maximum size
+     */
     @Test
     void getSize() {
+        ResourceDepot stash = new BasicDepot(3);
+        assertEquals(3, stash.getSize());
     }
 
+    /**
+     * This method tests the emptying of the storage's contents
+     */
     @Test
     void empty() {
+        ResourceDepot stash = new BasicDepot(3);
+        try {
+            stash.addResource(ResourceType.SHIELD, 2);
+        } catch (NotEnoughSpaceException | WrongResourceTypeException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        stash.empty();
+        assertEquals(0, stash.getNumOfResource(ResourceType.SHIELD));
     }
 
     /**
@@ -36,13 +101,37 @@ class BasicDepotTest {
      */
     @Test
     void removeResource() {
+        ResourceDepot stash = new BasicDepot(3);
+        try {
+            stash.addResource(ResourceType.SHIELD, 2);
+        } catch (NotEnoughSpaceException | WrongResourceTypeException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        try {
+            stash.removeResource(ResourceType.SHIELD, 1);
+        } catch (NotEnoughResourceException | ResourceNotPresentException ex) {
+            System.out.println(ex.getMessage());
+            fail();
+        }
+        assertEquals(1, stash.getNumOfResource(ResourceType.SHIELD));
     }
 
+    /**
+     * This method tests the reaction of the getNumOfResource method to an empty storage and the correct working of the constructor
+     */
     @Test
-    void getNumOfResource() {
+    void getNumOfResourceEmpty() {
+        ResourceDepot stash = new BasicDepot(3);
+        assertEquals(0, stash.getNumOfResource(ResourceType.SHIELD));
     }
 
+    /**
+     * This method tests the reaction of the getStoredResources method to an empty storage and the correct working of the constructor
+     */
     @Test
-    void getStoredResources() {
+    void getStoredResourcesEmpty() {
+        ResourceDepot stash = new BasicDepot(3);
+        assertTrue(stash.getStoredResources().isEmpty());
     }
 }
