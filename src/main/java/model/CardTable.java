27b5b6d0749203395d14f8171d6d69ceb8c9cf44
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-06 18:10:15 +0200

    Update CardTable.java

--- src/main/java/model/CardTable.java
+++ src/main/java/model/CardTable.java
@@ -1,4 +1,25 @@
 package model;
 
+import model.card.DevelopmentCard;
+
+import java.util.List;
+
 public class CardTable {
+    private List<List<DevelopmentCard>> greenCards;
+    private List<List<DevelopmentCard>> blueCards;
+    private List<List<DevelopmentCard>> yellowCards;
+    private List<List<DevelopmentCard>> purpleCards;
+
+    public void buyTopCard(CardColor color, int position, int idk, PlayerBoard playerBoard){
+        //TODO
+    }
+
+    public void discardTop(CardColor color, int idk){
+        //TODO
+    }
+
+    public boolean checkAllColorAvailable(){
+        //TODO
+        return false;
+    }
 }
