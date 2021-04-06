5dad521eadd9a9f0399d2391691e9286de8c0c4c
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-04-06 10:55:00 +0200

    Update ResourceDepot.java
    added addResource method

--- src/main/java/model/storage/ResourceDepot.java
+++ src/main/java/model/storage/ResourceDepot.java
@@ -1,5 +1,7 @@
 package model.storage;
 
+import Exceptions.NotEnoughSpaceException;
+import Exceptions.WrongResourceTypeException;
 import model.ResourceType;
 
 /**
@@ -7,6 +9,15 @@ import model.ResourceType;
  * It has methods for evaluating the depot's capacity or whether it is blocking a specific resource, and to empty the depot, as well as the standard methods for a ResourceStash.
  */
 public interface ResourceDepot extends ResourceStash {
+    /**
+     * Adds the given resource to the storage
+     * @param resource - the resource to be added
+     * @param quantity - the amount of resource to add to the amount stored
+     * @throws WrongResourceTypeException - if the type of the resource to be added cannot (currently) be added to the storage
+     * @throws NotEnoughSpaceException - if the quantity of the resource to be added plus the amount already stored exceeds the maximum capacity
+     */
+    void addResource (ResourceType resource, int quantity) throws WrongResourceTypeException, NotEnoughSpaceException;
+
     /**
      * Returns whether or not the depot, if it were empty, could contain the given amount of the given resource
      * @param resource - the resource to be stored
