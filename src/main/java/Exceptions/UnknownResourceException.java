34fe831e9562cb74078fd49652b13b8f41bb48e1
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-08 01:41:04 +0200

    Create UnknownResourceException.java

---
+++ src/main/java/Exceptions/UnknownResourceException.java
@@ -0,0 +1,18 @@
+package Exceptions;
+
+public class UnknownResourceException extends Exception{
+    private String unknownResourceLocation = null;
+
+    public UnknownResourceException(String unknownResourceLocation) {
+        this.unknownResourceLocation = unknownResourceLocation;
+    }
+
+    /**
+     * Returns the message describing the type of error that occurred
+     * @return - the message (String) of this exception
+     */
+    @Override
+    public String getMessage() {
+        return ("Error: There are still undefined resources in" + unknownResourceLocation + ".");
+    }
+}
