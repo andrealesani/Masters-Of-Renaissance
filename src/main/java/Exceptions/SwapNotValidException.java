2c74550c787f06ec153628df1435b7c3cb107e11
Tommaso Brumani <tommaso.brumani@mail.polimi.it>
2021-04-07 09:10:06 +0200

    Create SwapNotValidException.java

---
+++ src/main/java/Exceptions/SwapNotValidException.java
@@ -0,0 +1,15 @@
+package Exceptions;
+
+/**
+ * This Exception is thrown when two depots are attempted to be swapped, and the resources of one cannot be added to the other.
+ * */
+public class SwapNotValidException extends Exception {
+    /**
+     * Returns the message describing the type of error that occurred
+     * @return - the message (String) of this exception
+     */
+    @Override
+    public String getMessage() {
+        return ("Error: The contents of the two depots cannot be swapped.");
+    }
+}
