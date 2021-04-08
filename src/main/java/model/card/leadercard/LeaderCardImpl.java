0e78913a37dd0a79f9c8b7a286b12952d3a8ee5d
andrealesani <andrea.alesani@gmail.com>
2021-04-08 17:48:42 +0200

    Added victoryPoints attribute and getter for LeaderCardImpl

--- src/main/java/model/card/leadercard/LeaderCardImpl.java
+++ src/main/java/model/card/leadercard/LeaderCardImpl.java
@@ -6,7 +6,15 @@ import model.PlayerBoard;
  * Implementation of the LeaderCard interface
  */
 public class LeaderCardImpl implements LeaderCard {
+    private final int victoryPoints;
 
+    public LeaderCardImpl(int victoryPoints) {
+        this.victoryPoints = victoryPoints;
+    }
+
+    public int getVictoryPoints(){
+        return victoryPoints;
+    }
     @Override
     public void doAction(PlayerBoard playerBoard) {} /* this method should either be boolean or throw an exception */
 
