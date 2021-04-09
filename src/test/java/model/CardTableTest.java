5f7fa443170fc12d8498128963456e82b0752422
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-09 20:30:39 +0200

    Create CardTableTest.java

---
+++ src/test/java/model/CardTableTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTableTest {
+    @Test
+    void constructor(){
+        CardTable cardTable = new CardTable();
+    }
+}
