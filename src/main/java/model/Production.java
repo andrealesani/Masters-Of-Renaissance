c48fb925412eebee6a702aa074ced9d0b8e2d6d4
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-09 02:50:44 +0200

    CTRL ALT L on Production

--- src/main/java/model/Production.java
+++ src/main/java/model/Production.java
@@ -31,13 +31,14 @@ public class Production {
 
     /**
      * Equals
+     *
      * @param obj object to compare
      * @return true only if the 2 Productions input and output lists are equal
      */
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Production)
-            if(input.equals(((Production) obj).getInput()) && output.equals(((Production) obj).getInput()))
+        if (obj instanceof Production)
+            if (input.equals(((Production) obj).getInput()) && output.equals(((Production) obj).getInput()))
                 return true;
         return false;
     }
