e54266c3e653f446b0d56a24b87a1646c1b779c2
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-14 01:38:13 +0200

    Changed DevelopmentCard implementation to support creation from JSON

--- src/main/java/model/card/DevelopmentCard.java
+++ src/main/java/model/card/DevelopmentCard.java
@@ -16,8 +16,12 @@ import java.util.List;
 public class DevelopmentCard extends Card {
     private final CardColor color;
     private final int level;
-    private final List<ResourceType> cost;
-    private final Production production;
+    private final ResourceType[] costType;
+    private final int[] costQuantity;
+    private final ResourceType[] inputType;
+    private final int[] inputQuantities;
+    private final  ResourceType[] outputType;
+    private final int[] outputQuantities;
 
     /**
      * Constructor
@@ -27,72 +31,17 @@ public class DevelopmentCard extends Card {
      */
     public DevelopmentCard(int victoryPoints, int level, CardColor color,
                            ResourceType[] costType, int[] costQuantity,
-                           ResourceType[] inputType, int[] inputQuantity,
-                           ResourceType[] outputType, int[] outputQuantity) {
+                           ResourceType[] inputType, int[] inputQuantities,
+                           ResourceType[] outputType, int[] outputQuantities) {
         super(victoryPoints);
         this.level = level;
         this.color = color;
-
-        cost = initializeCost(costType, costQuantity);
-        production = initializeProduction(inputType, inputQuantity, outputType, outputQuantity);
-    }
-
-    private List<ResourceType> initializeCost(ResourceType[] costType, int[] costQuantity) {
-        List<ResourceType> costList = new ArrayList<>();
-        for (int i = 0; i < costType.length; i++) {
-            for (int j = 0; j < costQuantity[i]; j++) {
-                costList.add(costType[i]);
-            }
-        }
-        return costList;
-    }
-
-    private Production initializeProduction(ResourceType[] inputType, int[] inputQuantity, ResourceType[] outputType, int[] outputQuantity) {
-        List<Resource> input = new ArrayList<>();
-        List<Resource> output = new ArrayList<>();
-
-        Resource stone = new ResourceStone();
-        Resource coin = new ResourceCoin();
-        Resource shield = new ResourceShield();
-        Resource servant = new ResourceServant();
-        Resource unknown = new ResourceUnknown();
-        Resource faith = new ResourceFaith();
-
-        for (int i = 0; i < inputType.length; i++) {
-            for(int j = 0; j < inputQuantity[i]; j++) {
-                if (inputType[i] == ResourceType.SHIELD) {
-                    input.add(shield);
-                } else if (inputType[i] == ResourceType.COIN) {
-                    input.add(coin);
-                } else if (inputType[i] == ResourceType.STONE) {
-                    input.add(stone);
-                } else if (inputType[i] == ResourceType.SERVANT) {
-                    input.add(servant);
-                } else if (inputType[i] == ResourceType.UNKNOWN) {
-                    input.add(unknown);
-                }
-            }
-        }
-
-        for (int i = 0; i < inputType.length; i++) {
-            for(int j = 0; j < outputQuantity[i]; j++) {
-                if (outputType[i] == ResourceType.SHIELD) {
-                    output.add(shield);
-                } else if (outputType[i] == ResourceType.COIN) {
-                    output.add(coin);
-                } else if (outputType[i] == ResourceType.STONE) {
-                    output.add(stone);
-                } else if (outputType[i] == ResourceType.SERVANT) {
-                    output.add(servant);
-                } else if (inputType[i] == ResourceType.UNKNOWN) {
-                    input.add(unknown);
-                } else if (inputType[i] == ResourceType.FAITH) {
-                    input.add(faith);
-                }
-            }
-        }
-
-        return new Production(input, output);
+        this.costType = costType;
+        this.costQuantity = costQuantity;
+        this.inputType = inputType;
+        this.inputQuantities = inputQuantities;
+        this.outputType = outputType;
+        this.outputQuantities = outputQuantities;
     }
 
     /**
@@ -119,10 +68,13 @@ public class DevelopmentCard extends Card {
      * @return returns card cost
      */
     public List<ResourceType> getCost() {
-        List<ResourceType> costCpy = new ArrayList<>();
-        costCpy.addAll(cost);
-
-        return costCpy;
+        List<ResourceType> costList = new ArrayList<>();
+        for (int i = 0; i < costType.length; i++) {
+            for (int j = 0; j < costQuantity[i]; j++) {
+                costList.add(costType[i]);
+            }
+        }
+        return costList;
     }
 
     /**
@@ -131,7 +83,51 @@ public class DevelopmentCard extends Card {
      * @return returns the Production that this card enables for the player
      */
     public Production getProduction() {
-        return production;
+        List<Resource> input = new ArrayList<>();
+        List<Resource> output = new ArrayList<>();
+
+        Resource stone = new ResourceStone();
+        Resource coin = new ResourceCoin();
+        Resource shield = new ResourceShield();
+        Resource servant = new ResourceServant();
+        Resource unknown = new ResourceUnknown();
+        Resource faith = new ResourceFaith();
+
+        for (int i = 0; i < inputType.length; i++) {
+            for(int j = 0; j < inputQuantities[i]; j++) {
+                if (inputType[i] == ResourceType.SHIELD) {
+                    input.add(shield);
+                } else if (inputType[i] == ResourceType.COIN) {
+                    input.add(coin);
+                } else if (inputType[i] == ResourceType.STONE) {
+                    input.add(stone);
+                } else if (inputType[i] == ResourceType.SERVANT) {
+                    input.add(servant);
+                } else if (inputType[i] == ResourceType.UNKNOWN) {
+                    input.add(unknown);
+                }
+            }
+        }
+
+        for (int i = 0; i < inputType.length; i++) {
+            for(int j = 0; j < outputQuantities[i]; j++) {
+                if (outputType[i] == ResourceType.SHIELD) {
+                    output.add(shield);
+                } else if (outputType[i] == ResourceType.COIN) {
+                    output.add(coin);
+                } else if (outputType[i] == ResourceType.STONE) {
+                    output.add(stone);
+                } else if (outputType[i] == ResourceType.SERVANT) {
+                    output.add(servant);
+                } else if (inputType[i] == ResourceType.UNKNOWN) {
+                    input.add(unknown);
+                } else if (inputType[i] == ResourceType.FAITH) {
+                    input.add(faith);
+                }
+            }
+        }
+
+        return new Production(input, output);
     }
 
     /**
