1a7f13edc375f20456af6f4832a85864f02790ff
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-14 12:32:03 +0200

    Update UserInterfaceTest.java

--- src/test/java/model/UserInterfaceTest.java
+++ src/test/java/model/UserInterfaceTest.java
@@ -1,7 +1,98 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
+import java.beans.Transient;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInterfaceTest {
 
+    // GIGI SECTION
+    @Test
+    void chooseLeaderCard() {
+
+    }
+
+    @Test
+    void playLeaderCard() {
+
+    }
+
+    @Test
+    void discardLeaderCard() {
+
+    }
+
+    @Test
+    void selectFromMarket() {
+
+    }
+    // TOM SECTION
+     @Test
+     void sendResourceToDepot() {
+
+     }
+
+     @Test
+     void chooseMarbleConversion() {
+
+     }
+
+     @Test
+     void takeResourceFromWarehouseCard() {
+
+     }
+
+     @Test
+     void takeResourceFromStrongboxCard() {
+
+     }
+
+     @Test
+     void takeResourceFromWarehouseProduction() {
+
+     }
+
+     @Test
+     void takeResourceFromStrongboxProduction() {
+
+     }
+
+    // ANDRE SECTION
+    @Test
+    void buyDevelopmentCard() {
+
+    }
+
+    @Test
+    void selectProduction() {
+
+    }
+
+     @Test
+    void resetProductionChoice() {
+
+     }
+
+     @Test
+    void confirmProductionChoice() {
+
+     }
+
+     @Test
+    void chooseJollyInput() {
+
+     }
+
+     @Test
+    void chooseJollyOutput() {
+
+     }
+
+     // END TURN
+    @Test
+    void endTurn() {
+        
+    }
 }
