3958f5d5e4020be7240e6382e3a550cbedaf0d17
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-04-27 16:57:46 +0200

    Update Lobby.java

--- src/main/java/model/Lobby.java
+++ src/main/java/model/Lobby.java
@@ -7,23 +7,59 @@ import java.util.List;
  * This class represents the game's lobby
  */
 public class Lobby {
-    private List<String> playerLobby;
+    private final List<String> playerLobby = new ArrayList<>();
     private int nextGamePlayers;
-    private List<Game> activeGames;
+    private final List<Game> activeGames = new ArrayList<>();
 
-    private void createNewGame(){
+    /**
+     * Creates a Game with the number of players specified in nextGamePlayers. The logic used to choose the players
+     * that enter the Game is FIFO
+     */
+    public void createNewGame() {
         List<String> playersEnteringGame = new ArrayList<>();
-        for(int i=0; i<nextGamePlayers; i++){
-            playersEnteringGame.add(playerLobby.get(i));
+        for (int i = 0; i < nextGamePlayers; i++) {
+            playersEnteringGame.add(playerLobby.get(0));
+            playerLobby.remove(0);
         }
         activeGames.add(new Game(playersEnteringGame));
     }
 
-    public void addPlayer(String nickname){
+    /**
+     * Adds a player to the Lobby
+     *
+     * @param nickname is a String that specified the nickname the player has chosen
+     */
+    public void addPlayer(String nickname) {
         playerLobby.add(nickname);
     }
 
-    public void numberOfPlayers(int num){
+    /**
+     * Sets the number of players that the next Game will have
+     *
+     * @param num specifies the number of players for the next Game
+     */
+    public void setNextGamePlayers(int num) {
         nextGamePlayers = num;
+    }
+
+    /**
+     * This method is used to check that the username chosen by a new client is not currently used by any other client
+     * already connected to the server. This guarantees that every username is unique so that if a player disconnects
+     * from his game he can reconnect specifying his username
+     *
+     * @param newUser is the nickname that needs to be checked
+     * @return true if the nickname is not currently in use and can be assigned to the client
+     */
+    public boolean isUsernameAvailable(String newUser) {
+        for (String username : playerLobby) {
+            if (username.equals(newUser))
+                return false;
+        }
+        for (Game game : activeGames)
+            for (PlayerBoard player : game.getPlayersTurnOrder())
+                if (player.getUsername().equals(newUser))
+                    return false;
+
+        return true;
     }
 }
