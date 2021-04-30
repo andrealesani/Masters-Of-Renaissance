e3820f93573ace15d466ee27e48c40522601e159
Giggi98 <80157635+Giggi98@users.noreply.github.com>
2021-04-30 09:59:00 +0100

    add Class for Testing PBBean

---
+++ src/test/java/network/beans/PlayerBoardBeanTest.java
@@ -0,0 +1,55 @@
+package network.beans;
+
+import model.Game;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerBoardBeanTest {
+
+    @Test
+    void setWaitingRoomTypeFromPB() {
+        Game game = new Game();
+
+    }
+
+    @Test
+    void setWaitingRoomQuantitiesFromPB() {
+    }
+
+    @Test
+    void setStrongboxTypeFromPB() {
+    }
+
+    @Test
+    void setStrongboxQuantitiesFromPB() {
+    }
+
+    @Test
+    void setCardSlotsFromPB() {
+    }
+
+    @Test
+    void setLeaderCardsFromPB() {
+    }
+
+    @Test
+    void setActiveLeaderCardsFromPB() {
+    }
+
+    @Test
+    void setVpFaithTilesFromPB() {
+    }
+
+    @Test
+    void setVpFaithValuesFromPB() {
+    }
+
+    @Test
+    void setPopeTileStatesFromPB() {
+    }
+
+    @Test
+    void setPopeTilePointsFromPB() {
+    }
+}
