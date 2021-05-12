99e5c565551dcaace5990437b49e91a98953a511
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-05-12 15:54:13 +0200

    Update ClientReader.java

--- src/main/java/network/ClientReader.java
+++ src/main/java/network/ClientReader.java
@@ -1,7 +1,11 @@
 package network;
 
+import com.google.gson.Gson;
+import network.beans.MarketBean;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class ClientReader implements Runnable {
 
@@ -17,10 +21,15 @@ public class ClientReader implements Runnable {
     //MULTITHREADING METHODS
 
     public void run() {
+        Gson gson = new Gson();
         while (!doStop) {
             try {
                 String response = in.readLine();
-                System.out.println("Server says: " + response);
+                Map responseMap = gson.fromJson(response, Map.class);
+                if(responseMap.get("type").equals("INFO") || responseMap.get("type").equals("ERROR"))
+                    System.out.println(responseMap.get("jsonMessage"));
+                else
+                    System.out.println("Server says: " + response);
             } catch (IOException ex) {
                 System.out.println("Uh-oh, something went wrong while reading from the connection!");
             }
