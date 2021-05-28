8c64920f2ba8601fd70a9fb1d055a17480dd3aaa
andrealesani <80156743+andrealesani@users.noreply.github.com>
2021-05-28 13:25:18 +0200

    Update StaticMethods.java

--- src/main/java/network/StaticMethods.java
+++ src/main/java/network/StaticMethods.java
@@ -1,5 +1,7 @@
 package network;
 
+import model.Color;
+
 public class StaticMethods {
     public static void clearConsole() {
         try {
@@ -11,7 +13,7 @@ public class StaticMethods {
                 Runtime.getRuntime().exec("clear");
                 System.out.println("\033c");
             }
-            System.out.println("Hint: type 'help' for a list of commands you can do ;)");
+            System.out.println("\n" + Color.YELLOW_LIGHT_BG + Color.GREY_DARK_FG + "Hint:" + Color.RESET + " type '" + Color.RESOURCE_STD + "help" + Color.RESET + "' for a list of commands you can do ;)" + "\n");
         } catch (final Exception e) {
             System.out.println("Warning: failed to clear console");
         }
