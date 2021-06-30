5a0eed60c51138b6bb6a21b995ec2165b4e6ab48
Tommaso Brumani <tommaso.brumani@libero.it>
2021-06-30 02:24:16 +0200

    ADD IP SELECTION TO CLI + REBUILD JAVADOC

--- src/main/java/it/polimi/ingsw/client/CLI/CLI.java
+++ src/main/java/it/polimi/ingsw/client/CLI/CLI.java
@@ -26,46 +26,97 @@ public class CLI {
     public static void main(String[] args) {
 
         //Initialize hostName and portNumber
-        String hostName;
-        int portNumber;
+        Gson gson = new Gson();
+        Reader reader = new InputStreamReader(CLI.class.getResourceAsStream("/json/HostAndPort.json"), StandardCharsets.UTF_8);
+        Map map = gson.fromJson(reader, Map.class);
 
-        if (args != null && args.length > 0) {
-            hostName = args[0];
-            portNumber = Integer.parseInt(args[1]);
-        } else {
-            System.out.println("No parameters on command line: reading from Json.");
-            Gson gson = new Gson();
-            Reader reader = new InputStreamReader(CLI.class.getResourceAsStream("/json/HostAndPort.json"), StandardCharsets.UTF_8);
-            Map map = gson.fromJson(reader, Map.class);
-            hostName = (String) map.get("hostName");
-            portNumber = ((Double) map.get("portNumber")).intValue();
-        }
+        BufferedReader stdIn =
+                new BufferedReader(
+                        new InputStreamReader(System.in));
 
-        System.out.println("Attempting connection...");
-
-        //Attempts connection to server
-        Socket clientSocket = null;
-        try {
-            System.out.println("Attempting connection to " + hostName + "...");
-            clientSocket = new Socket(hostName, portNumber);
-        } catch (IOException ex) {
-            System.err.println("Server is not online");
-            System.exit(1);
-        }
+        //Creates the socket to connect with the server
+        Socket clientSocket = createSocket(stdIn, map);
 
         System.out.println("Client connected!");
 
-        startCLI(clientSocket);
+        //Starts the CLI
+        startCLI(clientSocket, stdIn);
     }
 
     //PRIVATE METHODS
 
     /**
+     * Asks the user's input to determine which host ip and port to connect to
+     *
+     * @param stdIn       the stream for the user's input
+     * @param hostAndPort the map containing the default host and port
+     * @return the socket for the connection with the server
+     */
+    private static Socket createSocket(BufferedReader stdIn, Map hostAndPort) {
+        String defaultHostName = (String) hostAndPort.get("hostName");
+        int defaultPortNumber = ((Double) hostAndPort.get("portNumber")).intValue();
+
+        String hostName;
+        int portNumber;
+
+        while (true) {
+
+            System.out.println("Please select which server to connect to: \n");
+
+            //Asks the player for the host ip
+
+            System.out.println( "Default host ip is '" + defaultHostName + "', " +
+                                "press ENTER to confirm or insert a new one to change it.");
+            try {
+
+                hostName = stdIn.readLine();
+                if (hostName.equals(""))
+                    hostName = defaultHostName;
+
+            } catch (IOException e) {
+                System.err.println("Error when trying to read host ip, using default.");
+                hostName = defaultHostName;
+            }
+
+            //Asks the player for the port
+
+            System.out.println( "Default host port is '" + defaultPortNumber + "', " +
+                                "press ENTER to confirm or insert a new one to change it.");
+            try {
+
+                String portString = stdIn.readLine();
+                if (portString.equals(""))
+                    portNumber = defaultPortNumber;
+                else
+                    portNumber = Integer.parseInt(portString);
+
+            } catch (IOException | NumberFormatException e) {
+                System.err.println("Error when trying to read host port, using default.");
+                portNumber = defaultPortNumber;
+            }
+
+            //Attempts connection to server
+
+            Socket clientSocket = null;
+            try {
+                System.out.println("Attempting connection to '" + hostName + ":" + portNumber + "'...");
+                clientSocket = new Socket(hostName, portNumber);
+                return clientSocket;
+            } catch (IOException ex) {
+                System.err.println("Selected server is not online.");
+            } catch (IllegalArgumentException ex) {
+                System.err.println("Invalid port number.");
+            }
+        }
+    }
+
+    /**
      * Start the Command Line Interface
      *
      * @param clientSocket the socket for the connection with the server
+     * @param stdIn        the stream taken from the user's input
      */
-    private static void startCLI(Socket clientSocket) {
+    private static void startCLI(Socket clientSocket, BufferedReader stdIn) {
         //Creates a countdown latch to ensure that the program shuts down if either the reading or writing threads fail
         CountDownLatch latch = new CountDownLatch(1);
 
@@ -73,7 +124,7 @@ public class CLI {
 
             //Sets a 10 second timeout for the socket reader
             try {
-                clientSocket.setSoTimeout(10*1000);
+                clientSocket.setSoTimeout(10 * 1000);
             } catch (SocketException e) {
                 System.err.println("Warning: couldn't set socket timeout in CLI");
                 e.printStackTrace();
@@ -85,9 +136,6 @@ public class CLI {
                             new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out =
                     new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader stdIn =
-                    new BufferedReader(
-                            new InputStreamReader(System.in));
 
             //Creates the object which stores the game data
             ClientView ClientView = new ClientView();
--- src/main/java/it/polimi/ingsw/server/ServerMain.java
+++ src/main/java/it/polimi/ingsw/server/ServerMain.java
@@ -25,17 +25,11 @@ public class ServerMain {
     public static void main(String[] args) {
 
         //Initialize portNumber
-        int portNumber = 0;
-        if (args != null && args.length > 0) {
-            portNumber = Integer.parseInt(args[0]);
-        } else {
-            System.out.println("No parameters found on command line: reading them from Json.");
-            Gson gson = new Gson();
+        Gson gson = new Gson();
 
-            Reader reader = new InputStreamReader(ServerMain.class.getResourceAsStream("/json/HostAndPort.json"), StandardCharsets.UTF_8);
-            Map map = gson.fromJson(reader, Map.class);
-            portNumber = ((Double) map.get("portNumber")).intValue();
-        }
+        Reader reader = new InputStreamReader(ServerMain.class.getResourceAsStream("/json/HostAndPort.json"), StandardCharsets.UTF_8);
+        Map map = gson.fromJson(reader, Map.class);
+        int portNumber = ((Double) map.get("portNumber")).intValue();
 
         //Start the server
         startServer(portNumber);
